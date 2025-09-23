65d179ae66959a112afe707276bad88354ef3d01
Matt Turner <maturner01@gmail.com>
2025-09-22 18:05:59 -0700

    expanded free agents post and added references.bib

--- posts/free-agents-adapt-better/R/model.R
+++ posts/free-agents-adapt-better/R/model.R
@@ -1,18 +1,19 @@
+##
+# posts/free-agents-adapt-better/model.R
+#
+# Modeling code to accompany week of 9/16 lecture "Free Agents Adapt Better than
+# Conformist Puppets".
+# 
+# Author: Matt Turner <matt@mat.phd>
+# Date: 2025-09-18
+#
 
 
+#-------- CONFORMITY ------------
 
-# Initialize the ABM and agents
-abm <- 
-  make_abm(graph = igraph::make_empty_graph(10)) |>
-  initialize_agents(initial_prevalence = 0.2)
-
-# Inspect the initialization to ensure 2 of 10 are A-doers
-plot_network_adoption(
-  abm, layout = igraph::in_circle(), 
-  plot_mod = \(p) p + ggtitle("Adoption at t = 0")
-)
-
-
+#' Create a conformity learning instance of ModelDynamics.
+#' 
+#' Returns socmod::ModelDynamics
 conformity_dynamics <- make_model_dynamics(
   
   # Partner selection is not necessary in conformity
@@ -45,6 +46,9 @@ conformity_dynamics <- make_model_dynamics(
 )
 
 
+#' Create an AgentBasedModel with conformity learning model dynamics.
+#' 
+#' @returns socmod::AgentBasedModel
 make_conformity_abm <- function(n_agents = 10, n_demonstrators = 5) {
   abm <- 
     make_abm(graph = igraph::make_empty_graph(n_agents), 
@@ -60,15 +64,71 @@ make_conformity_abm <- function(n_agents = 10, n_demonstrators = 5) {
 }
 
 
-single_conformity_trial <- function(n_agents = 10, n_demonstrators = 10) {
+#' Create a single conformity ABM then run a simulation trial on it.
+#' 
+#' @returns socmod::Trial
+single_conformity_trial <- function(n_agents = 10, 
+                                    n_demonstrators = 10,
+                                    stop = socmod::fixated) {
   
   abm <- make_conformity_abm(n_agents, n_demonstrators)
   
-  # Stop at fixation by default
-  trial <- run_trial(abm)
+  trial <- run_trial(abm, stop = stop)
   
   return (trial)
 }
 
-# 
-# tr1 <- single_conformity_trial()
+
+
+
+#-------- SUCCESS BIAS ------------
+
+# TODO modify conformity_dynamics below for success
+
+#' Create a conformity learning instance of ModelDynamics.
+#' 
+#' Returns socmod::ModelDynamics
+vicarity_dynamics <- make_model_dynamics(
+  
+  # Partner selection is adaptive; learners prefer more successful individuals
+  partner_selection = \(focal_agent, model) {
+    # Observe behaviors of randomly-chosen demonstrators
+    behaviors_sample <- sample(model$get_parameter("agent_payoffs"), 
+                               model$get_parameter("n_demonstrators"))
+    
+    return ()
+  },
+  
+  # Conformity interaction is not partner-based, so use dummy arg
+  interaction = \(focal_agent, ., model) {
+    
+    # Only need to set next behavior, payoffs irrelevant w/ conformity
+    focal_agent$set_next_behavior(next_behavior)
+  },
+  # Use the learning model stepper that makes "next" behavior/payoff "current"
+  model_step = \(model) {
+    learning_model_step(model)
+    
+    model$set_parameter("agent_behaviors", 
+                        purrr::map_chr(model$agents, \(a) a$get_behavior()))
+    
+  }
+)
+
+
+#' Create an AgentBasedModel with vicarity (success-biased) learning model dynamics.
+#' 
+#' @returns socmod::AgentBasedModel
+make_vicarity_abm <- function(n_agents = 10, n_demonstrators = 5) {
+  abm <- 
+    make_abm(graph = igraph::make_empty_graph(n_agents), 
+             model_dynamics = vicarity_dynamics,
+             n_demonstrators = n_demonstrators
+    ) |>
+    initialize_agents(initial_prevalence = 0.2)
+  
+  abm$set_parameter("agent_behaviors", 
+                    purrr::map_chr(abm$agents, \(a) a$get_behavior()))
+  
+  return (abm)
+}
